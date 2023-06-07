90723dc82964178acc224903a96feb08b2367fb6
allnorth <allnorth44@gmail.com>
2023-06-07 16:54:03 +0300

    fix commands

---
+++ exec2/commands.sh
@@ -0,0 +1,20 @@
+#сборка проекта
+docker compose build
+
+#запуск проекта
+docker compose up -d
+
+#перезапуск проекта
+docker compose restart
+
+#остановка проекта
+docker compose down
+
+#подключение к базе данных
+docker exec -it postgres psql -U postgres
+
+#логи с результатами выполнения python-скрипта
+docker logs app
+
+#запуск расчета
+docker restart app
